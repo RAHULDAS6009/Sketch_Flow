f1dc0908bd73870b925e972508b12e792ac395f5
RAHULDAS6009 <das206053@gmail.com>
2025-03-02 20:40:36 +0530

    gating the websocket server

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,28 @@
 import { WebSocketServer } from "ws";
+import jwt from "jsonwebtoken";
+import { jwt_secret } from "@repo/backend-common/types";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", function connection(ws) {
-  console.log("Web seocket Server is running on 8080");
-ws.send("something")
+wss.on("connection", function connection(ws, request) {
+  const url = request.url;
+  if (!url) {
+    return;
+  }
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token") || "";
+  const decoded = jwt.verify(token, jwt_secret);
+
+  if (typeof decoded == "string") {
+    ws.close();
+    return;
+  }
+  if (!decoded || !decoded.userId) {
+    ws.close();
+    return;
+  }
+
+  ws.on("message", (data) => {
+    ws.send("pong");
+  });
 });
